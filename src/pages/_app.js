eb419cbe427c0002a62deeca7059f4be6cc04086
ChauhanOO7 <saurabhc7388@gmail.com>
2024-09-25 15:04:09 +0530

    project commited

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+// _app.js
+import { MantineProvider } from '@mantine/core';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <MantineProvider theme={{ colorScheme: 'dark' }}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
+
+export default MyApp;
